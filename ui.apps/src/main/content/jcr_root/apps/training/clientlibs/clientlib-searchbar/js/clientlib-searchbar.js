efba360bd306424d474b192f168df66b9cae5ff2
Giuseppe Buono <g.buono@accenture.com>
2021-04-02 10:21:24 +0200

    Temporary commit

--- ui.apps/src/main/content/jcr_root/apps/training/clientlibs/clientlib-searchbar/js/clientlib-searchbar.js
+++ ui.apps/src/main/content/jcr_root/apps/training/clientlibs/clientlib-searchbar/js/clientlib-searchbar.js
@@ -2,39 +2,43 @@ const searchForm = document.querySelector("#search-form");
 
 searchForm.addEventListener("submit", event => {
     event.preventDefault();
-
-    fetch("/libs/granite/csrf/token.json", {method: "GET"})
-	.then(response => response.json())
-	.then(data => {
-        const params = {
-            searchQuery: document.querySelector("#search-query").value,
-            pagesLocation: searchForm.getAttribute("data-pages-location"),
-            assetsLocation: searchForm.getAttribute("data-assets-location")
-        };
-		
-		const headers = {
-            "Content-Type": "application/json",
-            "CSRF-Token": data.token,
-        }
-		
-        return fetch("/bin/search?" + new URLSearchParams(Object.entries(params)), {method: "POST", headers: headers});
-    })
-	.then(response => response.json())
-	.then(pagesList => {
-        if (!pagesList.length) {
-			document.querySelector('#search-results').innerText = searchForm.getAttribute("data-no-results-message");
-		}
-		else {
-			const searchResults = document.querySelector('#search-results');
+	
+	const searchQuery = document.querySelector("#search-query").value;
+	
+	if (searchQuery) {
+		fetch("/libs/granite/csrf/token.json", {method: "GET"})
+		.then(response => response.json())
+		.then(data => {
+	        const params = {
+	            searchQuery: document.querySelector("#search-query").value,
+	            pagesLocation: searchForm.getAttribute("data-pages-location"),
+	            assetsLocation: searchForm.getAttribute("data-assets-location")
+	        };
 			
-			for (let page of pagesList) {
-				const anchor = document.createElement('a');
-				const newLine = document.createElement('br');
-				anchor.innerText = page.title;
-				anchor.setAttribute('href', page.url + '.html');
-				searchResults.appendChild(anchor);
-				searchResults.appendChild(newLine);
+			const headers = {
+	            "Content-Type": "application/json",
+	            "CSRF-Token": data.token,
+	        }
+			
+	        return fetch("/bin/search?" + new URLSearchParams(Object.entries(params)), {method: "POST", headers: headers});
+	    })
+		.then(response => response.json())
+		.then(pagesList => {
+	        if (!pagesList.length) {
+				document.querySelector('#search-results').innerText = searchForm.getAttribute("data-no-results-message");
 			}
-		}
-    });
+			else {
+				const searchResults = document.querySelector('#search-results');
+				
+				for (let page of pagesList) {
+					const anchor = document.createElement('a'); 
+					const newLine = document.createElement('br'); 
+					anchor.innerText = page.title;
+					anchor.setAttribute('href', page.url + '.html');
+					searchResults.appendChild(anchor);
+					searchResults.appendChild(newLine);
+				}
+			}
+	    });	
+	}
 });
